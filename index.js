5802d7436b930f7661452da2d890b8a305461cd9
todderaf <79234395+todderaf@users.noreply.github.com>
2023-07-11 21:42:03 -0400

    More updates

--- index.js
+++ index.js
@@ -12,16 +12,19 @@ function handleAddClick(itemId) {
        return item.id == itemId
     })[0]
 
-    targetMenuItemObj.quantity++
-    console.log(targetMenuItemObj.quantity)
+    let quantity = ++targetMenuItemObj.quantity
+
+    render()
 
 }
 
 function getFeedHtml() {
     let feedHtml = ``
+    document.getElementById('checkout').innerHTML = ``
 
     menuArray.forEach((item) => {
         feedHtml += `
+
     <div class="menu-item-container">
         <div class="food-container" >
             <span class="emoji-size">${item.emoji}</span>
@@ -33,10 +36,28 @@ function getFeedHtml() {
          </div>
         <span class="add-btn"></span><i class="fa-regular fa-plus plus" data-add="${item.id}"></i></span>
     </div>
+
+    `
+        document.getElementById('checkout').innerHTML += `
+        <div class="hidden" id="item-${item.id}">
+            <div class="checkout-container">
+                 <div class="checkout-items-container">
+                    <h2>${item.name}</h2>
+                    <p class="remove-btn">remove</p>
+                 </div>
+                    <span id="calc-price-${item.id}"class="checkout-price">$${item.price * item.quantity}</spam>
+            </div>
+        </div>
         `
-
     })
-
+/*<div class="hidden" id="item-${item.id}"
+<div class="checkout-container">
+                    <div class="checkout-items-container">
+                        <h2>${item.name}</h2>
+                        <p class="remove-btn">remove</p>
+                    </div>
+                    <p class="checkout-price">${item.price * item.quantity</p>
+                </div>*/
     return feedHtml
 }
 
